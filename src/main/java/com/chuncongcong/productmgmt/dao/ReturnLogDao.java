65a2f36f3d065ad40276f4479ea89e114208f6a6
yyrely <734953453@qq.com>
2020-03-14 16:08:22 +0800

    修改bug

--- src/main/java/com/chuncongcong/productmgmt/dao/ReturnLogDao.java
+++ src/main/java/com/chuncongcong/productmgmt/dao/ReturnLogDao.java
@@ -1,8 +1,11 @@
 package com.chuncongcong.productmgmt.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.chuncongcong.productmgmt.model.dto.PurchaseLogDto;
 import com.chuncongcong.productmgmt.model.dto.TotalNumsDto;
 import com.chuncongcong.productmgmt.model.po.ReturnLogPo;
 import com.chuncongcong.productmgmt.model.vo.ReturnLogQueryVo;
@@ -15,7 +18,7 @@ import com.chuncongcong.productmgmt.model.vo.ReturnLogQueryVo;
 @Mapper
 public interface ReturnLogDao extends MyMapper<ReturnLogPo>{
 
-	void list(@Param("returnLogQueryVo") ReturnLogQueryVo returnLogQueryVo);
+	List<PurchaseLogDto> list(@Param("returnLogQueryVo") ReturnLogQueryVo returnLogQueryVo);
 
 	TotalNumsDto countNumsAndPrice(@Param("returnLogQueryVo") ReturnLogQueryVo returnLogQueryVo);
 }
