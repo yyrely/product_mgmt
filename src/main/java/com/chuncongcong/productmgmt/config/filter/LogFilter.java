27202be8756c8e561d5b42ae58a1132f36764eff
hujinyao <y734953453@gmail.com>
2021-07-19 15:49:36 +0800

    增加filter日志

---
+++ src/main/java/com/chuncongcong/productmgmt/config/filter/LogFilter.java
@@ -0,0 +1,30 @@
+package com.chuncongcong.productmgmt.config.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author HU
+ * @date 2021/7/19 15:36
+ */
+
+@Slf4j
+@Component
+public class LogFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+		log.info("[url : {}], [params : {}]", httpServletRequest.getRequestURL(), httpServletRequest.getQueryString());
+	}
+}
